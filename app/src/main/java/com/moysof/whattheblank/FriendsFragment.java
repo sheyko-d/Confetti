59f122f2d8ccdd6d23cdaa5015a4d4c1b784c4fa
sheyko_d <sheyko.dmitriy@gmail.com>
2015-07-09 22:05:32 +0300

    Added layouts for chat thread screen & friends screen

--- app/src/main/java/com/moysof/whattheblank/FriendsFragment.java
+++ app/src/main/java/com/moysof/whattheblank/FriendsFragment.java
@@ -1,12 +1,66 @@
 package com.moysof.whattheblank;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.moysof.whattheblank.adapter.FriendsAdapter;
+
 public class FriendsFragment extends Fragment {
+    private RecyclerView mRecycler;
+    private LinearLayoutManager mLayoutManager;
+    private FriendsAdapter mAdapter;
+    private SwipeRefreshLayout mRefreshLayout;
+    private SortedList mFriends = new SortedList<>(FriendsAdapter.Friend.class,
+            new SortedList.Callback<FriendsAdapter.Friend>() {
+                @Override
+                public int compare(FriendsAdapter.Friend o1, FriendsAdapter.Friend o2) {
+                    // Sort items, new ones first
+                    int i = o1.getType().compareTo(o2.getType());
+                    if (i != 0) return i;
+
+                    return o1.getName().compareTo(o2.getName());
+                }
+
+                @Override
+                public void onInserted(int position, int count) {
+                    mAdapter.notifyItemRangeInserted(position, count);
+                }
+
+                @Override
+                public void onRemoved(int position, int count) {
+                    mAdapter.notifyItemRangeRemoved(position, count);
+                }
+
+                @Override
+                public void onMoved(int fromPosition, int toPosition) {
+                    mAdapter.notifyItemMoved(fromPosition, toPosition);
+                }
+
+                @Override
+                public void onChanged(int position, int count) {
+                    mAdapter.notifyItemRangeChanged(position, count);
+                }
+
+                @Override
+                public boolean areContentsTheSame(FriendsAdapter.Friend oldItem, FriendsAdapter.Friend newItem) {
+                    // return whether the items' visual representations are the same or not.
+                    return oldItem.getType().equals(newItem.getType()) && oldItem.getName()
+                            .equalsIgnoreCase(oldItem.getName());
+                }
+
+                @Override
+                public boolean areItemsTheSame(FriendsAdapter.Friend item1, FriendsAdapter.Friend item2) {
+                    return item1.getUserId().equals(item2.getUserId());
+                }
+            });
 
     public static FriendsFragment newInstance() {
         FriendsFragment fragment = new FriendsFragment();
@@ -19,9 +73,59 @@ public class FriendsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        //View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
 
-        return new View(getActivity());
+        mRecycler = (RecyclerView) rootView.findViewById(R.id.friends_recycler_view);
+        mRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.friends_refresh_layout);
+
+        initRecycler();
+        initRefreshLayout();
+
+        return rootView;
+    }
+
+    private void initRecycler() {
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecycler.setLayoutManager(mLayoutManager);
+
+        mAdapter = new FriendsAdapter(getActivity(), mFriends);
+        mRecycler.setAdapter(mAdapter);
+
+        mFriends.clear();
+        mFriends.add(new FriendsAdapter.Friend(getString(R.string.friends_requests_header),
+                FriendsAdapter.ITEM_TYPE_REQUESTS_HEADER));
+        mFriends.add(new FriendsAdapter.Friend("0", "https://www.gravatar.com/avatar/ba0ebce7e385e4c242c9401ff9d551b2?s=328&d=identicon&r=PG&f=1", "Dan Trevor", "@dtizzle", FriendsAdapter.ITEM_TYPE_REQUESTS_FRIEND));
+        mFriends.add(new FriendsAdapter.Friend("1", "https://www.gravatar.com/avatar/ee6e12042dc31b1ef27471482f9ff91f?s=328&d=identicon&r=PG&f=1", "Sammy Davis", "@flyingsazzy", FriendsAdapter.ITEM_TYPE_REQUESTS_FRIEND));
+        mFriends.add(new FriendsAdapter.Friend(getString(R.string.friends_friends_header),
+                FriendsAdapter.ITEM_TYPE_FRIENDS_HEADER));
+        mFriends.add(new FriendsAdapter.Friend("0", "https://www.gravatar.com/avatar/ba0ebce7e385e4c242c9401ff9d551b2?s=328&d=identicon&r=PG&f=1", "Dan Trevor", "@dtizzle", FriendsAdapter.ITEM_TYPE_FRIENDS_FRIEND));
+        mFriends.add(new FriendsAdapter.Friend("1", "https://www.gravatar.com/avatar/ee6e12042dc31b1ef27471482f9ff91f?s=328&d=identicon&r=PG&f=1", "Sammy Davis", "@flyingsazzy", FriendsAdapter.ITEM_TYPE_FRIENDS_FRIEND));
+        mFriends.add(new FriendsAdapter.Friend(getString(R.string.friends_facebook_header),
+                FriendsAdapter.ITEM_TYPE_FACEBOOK_HEADER));
+        mFriends.add(new FriendsAdapter.Friend("0", "https://www.gravatar.com/avatar/ba0ebce7e385e4c242c9401ff9d551b2?s=328&d=identicon&r=PG&f=1", "Dan Trevor", "@dtizzle", FriendsAdapter.ITEM_TYPE_FACEBOOK_FRIEND));
+        mFriends.add(new FriendsAdapter.Friend("1", "https://www.gravatar.com/avatar/ee6e12042dc31b1ef27471482f9ff91f?s=328&d=identicon&r=PG&f=1", "Sammy Davis", "@flyingsazzy", FriendsAdapter.ITEM_TYPE_FACEBOOK_FRIEND));
+    }
+
+    private void initRefreshLayout() {
+        mRefreshLayout.setColorSchemeResources(R.color.primary, R.color.green, R.color.red,
+                R.color.blue);
+
+        mRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mRefreshLayout.setRefreshing(false);
+                            }
+                        });
+                    }
+                }, 2000);
+            }
+        });
     }
 
 }
---
+++ app/src/main/java/com/moysof/whattheblank/adapter/FriendsAdapter.java
@@ -0,0 +1,161 @@
+package com.moysof.whattheblank.adapter;
+
+import android.content.Context;
+import android.support.v7.util.SortedList;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.moysof.whattheblank.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.display.FadeInBitmapDisplayer;
+
+public class FriendsAdapter extends
+        RecyclerView.Adapter<FriendsAdapter.FriendHolder> {
+
+    public static final int ITEM_TYPE_REQUESTS_HEADER = 0;
+    public static final int ITEM_TYPE_REQUESTS_FRIEND = 1;
+    public static final int ITEM_TYPE_FRIENDS_HEADER = 2;
+    public static final int ITEM_TYPE_FRIENDS_FRIEND = 3;
+    public static final int ITEM_TYPE_FACEBOOK_HEADER = 4;
+    public static final int ITEM_TYPE_FACEBOOK_FRIEND = 5;
+    private ImageLoader mImageLoader;
+    private SortedList<Friend> friends;
+
+    public FriendsAdapter(Context context, SortedList<Friend> friends) {
+        this.friends = friends;
+
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder().cacheInMemory(true)
+                .cacheOnDisk(true)
+                .displayer(new FadeInBitmapDisplayer(250, true, false, false))
+                .showImageOnLoading(android.R.color.white)
+                .build();
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(
+                context).defaultDisplayImageOptions(defaultOptions).build();
+        ImageLoader.getInstance().init(config);
+        mImageLoader = ImageLoader.getInstance();
+    }
+
+    public static class Friend {
+
+        public String userId;
+        public String avatar;
+        public String name;
+        public String username;
+        public Integer type;
+
+        public Friend(String userId, String avatar, String name, String username, Integer type) {
+            this.userId = userId;
+            this.avatar = avatar;
+            this.name = name;
+            this.username = username;
+            this.type = type;
+        }
+
+        public Friend(String name, Integer type) {
+            this.name = name;
+            this.type = type;
+        }
+
+        public String getUserId() {
+            return userId;
+        }
+
+        public String getAvatar() {
+            return avatar;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getUsername() {
+            return username;
+        }
+
+        public Integer getType() {
+            return type;
+        }
+
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return friends.get(position).getType();
+    }
+
+    public class FriendHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public ImageView avatarImg;
+        public TextView nameTxt;
+        public TextView usernameTxt;
+        public TextView timeTxt;
+
+        public FriendHolder(View v) {
+            super(v);
+            avatarImg = (ImageView) v.findViewById(R.id.friends_img);
+            nameTxt = (TextView) v.findViewById(R.id.friends_name_txt);
+            usernameTxt = (TextView) v.findViewById(R.id.friends_username_txt);
+
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            friendClickListener.onItemClick(v, getAdapterPosition());
+        }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public FriendHolder onCreateViewHolder(ViewGroup parent,
+                                           int viewType) {
+        if (isItem(viewType)) {
+            return new FriendHolder(LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_friends, parent, false));
+        } else {
+            return new FriendHolder(LayoutInflater.from(parent.getContext()).inflate(
+                    R.layout.item_friends_subheader, parent, false));
+        }
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(FriendHolder holder, int position) {
+        Friend friend = friends.get(position);
+
+        holder.nameTxt.setText(friend.getName());
+        if (isItem(getItemViewType(position))) {
+            mImageLoader.displayImage(friend.getAvatar(), holder.avatarImg);
+            holder.usernameTxt.setText(friend.getUsername());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return friends.size();
+    }
+
+    OnItemClickListener friendClickListener = new OnItemClickListener() {
+
+        @Override
+        public void onItemClick(View v, int position) {
+        }
+
+    };
+
+    private Boolean isItem(int type) {
+        return (type == ITEM_TYPE_REQUESTS_FRIEND
+                || type == ITEM_TYPE_FRIENDS_FRIEND
+                || type == ITEM_TYPE_FACEBOOK_FRIEND);
+    }
+}
